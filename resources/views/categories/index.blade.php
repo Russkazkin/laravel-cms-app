c4231e18d580631cb060dc864a4791b271adbdca
skazkin <russkazkin@gmail.com>
2020-03-22 11:18:12 +0300

    Add base modal and script section

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -28,11 +28,42 @@
                     <a href="{{ route('categories.edit', $category->id)}}"
                        class="btn btn-info btn-sm"
                        role="button">Edit</a>
+                    <button type="button" class="btn btn-danger btn-sm"
+                            onclick="handleDelete()">
+                        Delete
+                    </button>
                 </td>
             </tr>
             @endforeach
             </tbody>
         </table>
+        <div class="modal fade"
+             id="deleteModal" tabindex="-1"
+             role="dialog"
+             aria-labelledby="deleteModalLabel"
+             aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+
 @endsection
