95a4190be32a56ea6fab1afb1feb769beb9e6f67
skazkin <russkazkin@gmail.com>
2020-03-24 19:26:13 +0300

    Add create post form

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="card card-default">
+    <div class="card-header">
+        Create post
+    </div>
+    <div class="card-body">
+        <form action="{{ route('posts.store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" id="title" name="title" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea name="description" id="description" cols="5" rows="5" class="form-control"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="content">Content</label>
+                <textarea name="content" id="content" cols="5" rows="5" class="form-control"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="published_at">Published at</label>
+                <input type="text" id="published_at" name="published_at" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="image">Image</label>
+                <input type="file" id="image" name="image" class="form-control">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-success">Create post</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+@endsection
