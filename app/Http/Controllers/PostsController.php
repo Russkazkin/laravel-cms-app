f5c55ab91e0392325ec5ec9c8ca3c966d33250a5
skazkin <russkazkin@gmail.com>
2020-03-24 19:09:40 +0300

    Add posts.index view

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -13,7 +14,7 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
+        return view('posts.index')->with('posts', Post::all());
     }
 
     /**
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="d-flex justify-content-end mb-2">
+    <a href="{{ route('posts.create') }}" class="btn btn-success">
+        Add Post
+    </a>
+</div>
+<div class="card card-default">
+    <div class="card-header">
+        Posts
+    </div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+            <tr>
+                <th>
+                    Name
+                </th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($posts as $post)
+                <tr>
+                    <td>{{ $post->name }}</td>
+                    <td>
+                        <a href="{{ route('posts.edit', $post->id)}}"
+                           class="btn btn-info btn-sm"
+                           role="button">Edit</a>
+                        <button type="button" class="btn btn-danger btn-sm"
+                                onclick="handleDelete({{ $post->id }})">
+                            Delete
+                        </button>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+@endsection
