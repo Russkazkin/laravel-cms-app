1b2751e8cbe0162367a5f1346077636b152f551d
skazkin <russkazkin@gmail.com>
2020-04-01 16:08:08 +0300

    Add trashe posts menu link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,12 +82,17 @@
                 @auth
                     <div class="row">
                         <div class="col-md-4">
-                            <ul class="list-group">
+                            <ul class="list-group mb-5">
                                 <li class="list-group-item">
                                     <a href="{{ route('posts.index') }}">Posts</a>
                                 </li>
                                 <li class="list-group-item">
                                     <a href="{{ route('categories.index') }}">Categories</a>
+                                </li>
+                            </ul>
+                            <ul class="list-group">
+                                <li class="list-group-item">
+                                    <a href="{{ route('posts.index') }}">Trashed posts</a>
                                 </li>
                             </ul>
                         </div>
