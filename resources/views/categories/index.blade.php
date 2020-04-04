e3da2b38f4e6a2d56dd870448ac8cd1956ba7d10
skazkin <russkazkin@gmail.com>
2020-04-04 10:37:39 +0300

    Add no categories  message

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,3 +1,7 @@
+<?php /**
+ * @var $categories \App\Category []
+ */ ?>
+
 @extends('layouts.app')
 
 @section('content')
@@ -11,6 +15,7 @@
         Categories
     </div>
     <div class="card-body">
+        @if($categories->count())
         <table class="table">
             <thead>
             <tr>
@@ -37,35 +42,38 @@
             @endforeach
             </tbody>
         </table>
-        <div class="modal fade"
-             id="deleteModal" tabindex="-1"
-             role="dialog"
-             aria-labelledby="deleteModalLabel"
-             aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <form action="" method="POST" id="deleteCategoryForm">
-                    @csrf
-                    @method('DELETE')
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Delete category</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="text-center">
-                            Are you sure you want to delete this category?
+        @else
+            <h6 class="text-center">No categories yet</h6>
+            <div class="modal fade"
+                 id="deleteModal" tabindex="-1"
+                 role="dialog"
+                 aria-labelledby="deleteModalLabel"
+                 aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <form action="" method="POST" id="deleteCategoryForm">
+                        @csrf
+                        @method('DELETE')
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Delete category</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="text-center">
+                                    Are you sure you want to delete this category?
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">No, go back</button>
+                                <button type="submit" class="btn btn-danger">Yes, delete</button>
+                            </div>
                         </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">No, go back</button>
-                        <button type="submit" class="btn btn-danger">Yes, delete</button>
-                    </div>
+                    </form>
                 </div>
-                </form>
             </div>
-        </div>
+        @endif
     </div>
 </div>
 @endsection
