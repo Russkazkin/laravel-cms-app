8f314b8739bec0eb321f740a84e5a244cfbbb4ba
skazkin <russkazkin@gmail.com>
2020-04-04 13:38:05 +0300

    Add image deleting after post permanently delete

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Posts\CreatePostsRequest;
 use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PostsController extends Controller
 {
@@ -93,9 +94,13 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
+        /**
+         * @var $post Post
+         */
         $post = Post::withTrashed()->where('id', $id)->firstOrFail();
 
         if($post->trashed()) {
+            Storage::delete($post->image);
             $post->forceDelete();
             session()->flash('success', 'Post deleted successfully');
         } else {
