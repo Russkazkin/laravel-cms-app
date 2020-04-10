0b7f45a5fa5b2b812e6888428064f3dc7b7f76aa
skazkin <russkazkin@gmail.com>
2020-04-10 06:36:51 +0300

    Complete VerifyCategoriesCount middleware

--- app/Http/Middleware/VerifyCategoriesCount.php
+++ app/Http/Middleware/VerifyCategoriesCount.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Category;
 use Closure;
 
 class VerifyCategoriesCount
@@ -15,6 +16,12 @@ class VerifyCategoriesCount
      */
     public function handle($request, Closure $next)
     {
+        if(!Category::all()->count()) {
+
+            session()->flash('warning', 'Please create at least one category');
+
+            return redirect(route('categories.create'));
+        }
         return $next($request);
     }
 }
