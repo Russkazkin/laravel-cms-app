8ce31fcef25100ad1eb4ef17e19797fd28c317e4
Russkazkin <ruslan@skazkin.su>
2020-05-21 11:37:00 +0300

    Add user info into single post page

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -23,8 +23,11 @@
                 <h1 class="display-4 mt-7 mb-8">
                     {{ $post->title }}
                 </h1>
-                <p><span class="opacity-70 mr-1">By</span> <a class="text-white" href="#">Hossein Shams</a></p>
-                <p><img class="avatar avatar-sm" src="../assets/img/avatar/2.jpg" alt="..."></p>
+                <p><span class="opacity-70 mr-1">By</span> <a class="text-white" href="#">
+                        {{ $post->user->name }}
+                    </a>
+                </p>
+                <p><img class="avatar avatar-sm" src="{{ Gravatar::src($post->user->email) }}" alt="..."></p>
 
             </div>
 
