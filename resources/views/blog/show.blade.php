a29e30ff5460180cda204f493ac177807f4ec078
Russkazkin <ruslan@skazkin.su>
2020-05-21 11:05:24 +0300

    Add some dinamic content to single post page

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -11,14 +11,18 @@
 @endsection
 
 @section('header')
-<header class="header text-white h-fullscreen pb-80" style="background-image: url(../assets/img/thumb/5.jpg);" data-overlay="9">
+<header class="header text-white h-fullscreen pb-80" style="background-image: url({{ asset("storage/$post->image") }});" data-overlay="9">
     <div class="container text-center">
 
         <div class="row h-100">
             <div class="col-lg-8 mx-auto align-self-center">
 
-                <p class="opacity-70 text-uppercase small ls-1">Product</p>
-                <h1 class="display-4 mt-7 mb-8">New published books to read by a product designer</h1>
+                <p class="opacity-70 text-uppercase small ls-1">
+                    {{ $post->category->name }}
+                </p>
+                <h1 class="display-4 mt-7 mb-8">
+                    {{ $post->title }}
+                </h1>
                 <p><span class="opacity-70 mr-1">By</span> <a class="text-white" href="#">Hossein Shams</a></p>
                 <p><img class="avatar avatar-sm" src="../assets/img/avatar/2.jpg" alt="..."></p>
 
